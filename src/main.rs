82364577fd502caca71e0f458f96096e471118e9
Alexander Tesfamichael <alex.tesfamichael@gmail.com>
2019-11-19 00:30:10 +0100

    WIP: Add AVL trees

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use avl_tree::AVLTree;
+
+mod avl_tree {
+    #[derive(Debug)]
+    struct Node<T> {
+        value: T
+    }
+    #[derive(Debug)]
+    pub struct AVLTree<T> {
+        root: Option<Node<T>>
+    }
+
+    impl<T> AVLTree<T> {
+        pub fn new() -> AVLTree<T> {
+            AVLTree { root: None }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello World!");
+    let store: AVLTree<String> = AVLTree::new();
+    println!("{:?}", store);
 }
